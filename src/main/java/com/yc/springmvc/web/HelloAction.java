bc126b0e08f4834b5af625a823d48720008a7139
Khao-thin <2316377605@qq.com>
2020-08-15 19:58:53 +0800

    8/15晚

--- src/main/java/com/yc/springmvc/web/HelloAction.java
+++ src/main/java/com/yc/springmvc/web/HelloAction.java
@@ -2,7 +2,12 @@ package com.yc.springmvc.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @Controller注解，表示该类所有的方法返回值是跳转到某个页面
+ * @RestController注解，表示该类所有的方法返回值都是JSON数据
+ * @author 23163
+ *
+ */
 @RestController
 public class HelloAction {
 	@RequestMapping("hello")
--- src/main/java/com/yc/springmvc/web/ProductAction.java
+++ src/main/java/com/yc/springmvc/web/ProductAction.java
@@ -1,5 +1,72 @@
 package com.yc.springmvc.web;
 
-public class ProductAction {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.yc.springmvc.bean.DmCart;
+import com.yc.springmvc.bean.DmUser;
+
+@RestController
+public class ProductAction {
+	@RequestMapping(path="product.do",params="op=query")
+	public String query() {
+		return "query";
+	}
+	@RequestMapping(path="product.do",params="op=add")
+	public String add() {
+		return "add";
+	}
+	@RequestMapping(path="product.do",params="op=mod",method = RequestMethod.POST)
+	public String mod() {
+		return "mod";
+	}
+	@RequestMapping(path="product.do",params="op=mgr",headers = {"Host=localhost","Cookie"})
+	public String mgr() {
+		return "mgr";
+	}
+	
+	
+	/**
+	 * SpringMVC 对于简单请求参数：数字String Boolean
+	 * 可以直接将请求参数注入到方法参数中
+	 * 必须满足：1、方法参数名==请求参数名
+	 * 		  2、类型要兼容 int num==>num==123
+	 */
+	@GetMapping("login.do")
+	public String login(String user,String password) {
+		return user+":"+password;
+	}
+	/**
+	 * 如果对象接收请求参数，对象的属性名与请求参数名称映射
+	 * 请求参数===>对象  装箱  ===>java包装类装箱
+	 * 对于一些特殊对象（日期），需要用springMVC转换器进行值的转换
+	 */
+	@GetMapping("reg.do")
+	public String login(DmUser user,DmCart cart) {
+		return user.toString()+"<br>"+cart;
+	}
+	
+	@GetMapping("pay.do")
+	public String pay(@RequestParam(
+			name="userid",required = true
+			)
+			String uid,Double money) {
+		return uid+":"+money;
+	}
+	
+	@GetMapping("findUser.do")
+	public DmUser findUser() {
+		DmUser user=new DmUser();
+		user.setCname("dsad");
+		user.setId(100);
+		user.setPassword("123");
+		return user;
+	}
+	
+	
+	
+	
 }
